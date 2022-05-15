7679e5144e52b0df19dc476117f004022d8c9a2e
unknown <vn.emploi@laposte.net>
2022-05-15 18:04:59 +0200

    Create Flashcard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import FlashcardList from './FlashcardList';
+import './app.css'
+import axios from 'axios';
 
 function App() {
+
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10&category=31')
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: decodeString(questionItem.correct_answer)
+          }
+        }))
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
     </div>
   );
 }
